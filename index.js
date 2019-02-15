fa131b041556ffa9cc17ede9f377053026cfa447
Perry Steward <perryjsteward@gmail.com>
2019-02-15 14:09:17 +0000

    fixed majority of eslint tests and everything working

--- index.js
+++ index.js
@@ -1,146 +1,142 @@
-var calc = (() => {
-    'use strict';
-  
-    var hookCallback;
+const calc = (() => {
+  let hookCallback;
 
-    const AVAILABLE_ACTIONS = ['MULTIPLY', 'ADD', 'MINUS', 'DIVIDE'];
+  const AVAILABLE_ACTIONS = ['MULTIPLY', 'ADD', 'MINUS', 'DIVIDE'];
 
-    function hooks () {
-        return hookCallback.apply(null, arguments);
+  function hooks() {
+    return hookCallback(...arguments);
+  }
+
+  // used to allow users to pass in an initial integer i.e. calc(4).add(5)
+  function _setHookCallback(callback) {
+    hookCallback = callback;
+  }
+
+  function _createFromConfig(currVal) {
+    return new Calculator(currVal);
+  }
+
+  function _setInitialValue(initVal) {
+    const val = initVal === undefined || initVal === null ? 0 : initVal;
+    if (_hasInitialValue.call(this)) {
+      throw new Error(`An initial value has already been set (${this.initialValue}). Try using reset().`);
     }
+    this.initialValue = _isNumber(val) ? val : null;
+  }
 
-    // used to allow users to pass in an initial integer i.e. calc(4).add(5)
-    function _setHookCallback (callback) {
-        hookCallback = callback;
+  function _setNextValue(value) {
+    if (_hasNextValue.call(this)) {
+      throw new Error(`A secondary value has already been set (${this.nextValue}). Try using reset().`);
     }
+    this.nextValue = _isNumber(value) ? value : null;
+  }
 
-    function _createFromConfig(currVal){
-        return new Calculator(currVal);     
+  function _isNumber(value) {
+    const isNumber = Number.isInteger(value);
+    if (!isNumber) {
+      throw new Error(`${value} is not a number, please provide an integer.`);
     }
+    return isNumber;
+  }
 
-    function _setInitialValue(initVal){
-        const val = initVal === undefined || initVal === null ? 0 : initVal;
-        if(_hasInitialValue.call(this)) { 
-            throw new Error(`An initial value has already been set (${this.initialValue}). Try using reset().`);
-        }
-        this.initialValue = _isNumber(val) ? val : null;
+  function _hasInitialValue() {
+    const val = this.initialValue;
+    return val !== undefined && _isNumber(val);
+  }
+
+  function _hasNextValue() {
+    const val = this.nextValue;
+    return val !== undefined && _isNumber(val);
+  }
+
+  function _hasExpressionValues() {
+    const hasBothValues = _hasInitialValue.call(this) && _hasNextValue.call(this);
+    if (!hasBothValues) {
+      throw new Error(`Please provide the calculatory an initial value (${this.initialValue}) and a secondary value (${this.nextValue}).`);
     }
+    return hasBothValues;
+  }
 
-    function _setNextValue(value){
-        if(_hasNextValue.call(this)) { 
-            throw new Error(`A secondary value has already been set (${this.nextValue}). Try using reset().`);
-        }
-        this.nextValue = _isNumber(value) ? value : null;
+  function _setCalculatorMethod(method) {
+    if (!method) {
+      throw new Error('An method is required for the calculator, please check the method to see if one has been passed');
     }
-
-    function _isNumber(value){
-        var isNumber = Number.isInteger(value);
-        if(!isNumber){
-            throw new Error(`${value} is not a number, please provide an integer.`);
-        }
-        return isNumber;
+    if (!AVAILABLE_ACTIONS.includes(method)) {
+      throw new Error(`The method "${method}" is not in the list of available calculator actions.`);
     }
+    this.method = method;
+  }
 
-    function _hasInitialValue(){
-        const val = this.initialValue;
-        return val !== undefined && _isNumber(val);
+  function _calculatorAction(method, callback) {
+    _setCalculatorMethod.call(this, method);
+    if (_hasExpressionValues.call(this)) {
+      callback(this.initialValue, this.nextValue);
     }
+  }
 
-    function _hasNextValue(){
-        const val = this.nextValue;
-        return val !== undefined && _isNumber(val);
-    }
+  // Calcs constructor
+  function Calculator(initValue) {
+    this.initialValue;
+    this.nextValue;
+    this.method;
+    this.result;
+    _setInitialValue.call(this, initValue);
+  }
 
-    function _hasExpressionValues(){
-        var hasBothValues = _hasInitialValue.call(this) && _hasNextValue.call(this);
-        if(!hasBothValues){
-            throw new Error(`Please provide the calculatory an initial value (${this.initialValue}) and a secondary value (${this.nextValue}).`);
-        };
-        return hasBothValues;
+  function multiply(nextValue) {
+    let result;
+    _setNextValue.call(this, nextValue);
+    _calculatorAction.call(this, 'MULTIPLY', (a, b) => {
+      result = (a * b);
+    });
+    return result;
+  }
 
-    }
+  function add(nextValue) {
+    let result;
+    _setNextValue.call(this, nextValue);
+    _calculatorAction.call(this, 'ADD', (a, b) => {
+      result = (a + b);
+    });
+    return result;
+  }
 
-    function _setCalculatorMethod(method){
-        if(!method){
-            throw new Error(`An method is required for the calculator, please check the method to see if one has been passed`);
-        };
-        if(!AVAILABLE_ACTIONS.includes(method)){
-            throw new Error(`The method "${method}" is not in the list of available calculator actions.`);
-        };
-        this.method = method;
-    }
+  function minus(nextValue) {
+    let result;
+    _setNextValue.call(this, nextValue);
+    _calculatorAction.call(this, 'MINUS', (a, b) => {
+      result = (a - b);
+    });
+    return result;
+  }
 
-    function _calculatorAction(method, callback){
-        _setCalculatorMethod.call(this, method);
-        if(_hasExpressionValues.call(this)){
-            callback(this.initialValue, this.nextValue);
-        }
-    }
+  function divide(nextValue) {
+    let result;
+    _setNextValue.call(this, nextValue);
+    _calculatorAction.call(this, 'DIVIDE', (a, b) => {
+      result = (a / b);
+    });
+    return result;
+  }
 
-    // Calcs constructor
-    function Calculator(initValue){
-        this.initialValue;
-        this.nextValue;
-        this.method;
-        this.result;
-        _setInitialValue.call(this, initValue);
-    }
+  function reset() {
+    this.initialValue = null;
+    this.nextValue = null;
+    this.action = null;
+  }
 
-    function multiply(nextValue){   
-        var result;
-        _setNextValue.call(this, nextValue);
-        _calculatorAction.call(this, 'MULTIPLY', (a, b) => {
-            result = (a * b);
-        });
-        return result;
-    }
+  const proto = Calculator.prototype;
 
-    function add(nextValue){
-        var result;
-        _setNextValue.call(this, nextValue);
-        _calculatorAction.call(this, 'ADD', (a, b) => {
-            result = (a + b);
-        });
-        return result;
-    }
+  // public API's
+  proto.multiply = multiply;
+  proto.add = add;
+  proto.divide = divide;
+  proto.reset = reset;
+  proto.minus = minus;
 
-    function minus(nextValue){
-        var result;
-        _setNextValue.call(this, nextValue);
-        _calculatorAction.call(this, 'MINUS', (a, b) => {
-            result = (a - b);
-        });
-        return result;
-    }
+  _setHookCallback(_createFromConfig);
 
-    function divide(nextValue){
-        var result;
-        _setNextValue.call(this, nextValue);
-        _calculatorAction.call(this, 'DIVIDE', (a, b) => {
-            result = (a / b);
-        });
-        return result;
-    }
-
-    function reset(){
-        this.initialValue = null;
-        this.nextValue = null;
-        this.action = null;
-    }
-
-    var proto = Calculator.prototype;
-
-    // public API's
-    proto.multiply = multiply;
-    proto.add = add;
-    proto.divide = divide;
-    proto.reset = reset;
-    proto.minus = minus;
-
-    _setHookCallback(_createFromConfig);
-    return hooks;
-
+  return hooks;
 })();
 
-typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = calc :
-typeof define === 'function' && define.amd ? define(calc): null;
+module.exports = calc;
